6b70fb8014902510bd4fc577fc277b20c084505c
Marijn Kieboom <ocmkieboom@gmail.com>
2024-11-08 20:59:42 +0100

    More progress

--- src/RNN/Decoder.py
+++ src/RNN/Decoder.py
@@ -22,8 +22,11 @@ class Decoder(nn.Module):
 
         self.embeddings = nn.Embedding(vocab_size, emb_dim)
         self.attention = Attention(num_layers, hidden_size)
+
+        self.input_fc = nn.Linear( emb_dim + hidden_size * 2, hidden_size)
+
         self.rnn = nn.GRU(
-            emb_dim + hidden_size * 2,
+            hidden_size,
             hidden_size,
             num_layers,
             batch_first=True,
@@ -48,32 +51,36 @@ class Decoder(nn.Module):
         """
 
         # Get learnable multi-dimensional embeddings of the input token
-        embeddings = self.dropout(self.embeddings(input)) # embeddings = [batch_size, embedding_dim]
+        embeddings = self.dropout(self.embeddings(input)) # embeddings: [batch_size, embedding_dim]
 
         # Compute context vector using the current hidden state (query) and encoder outputs (values)
         context_vector, att_weights = self.attention(hidden, encodings)
-        # context_vector = [batch_size, hidden size * 2]
-        # att_weights    = [batch_size, seq_len]
+        # context_vector: [batch_size, 2 * hidden_size]
+        # att_weights: [batch_size, seq_len] - for visualization purposes only
 
-        # Prepare input for the RNN by concatenating embeddings and context vector
-        rnn_input = torch.cat((embeddings, context_vector), dim=1)
-        # rnn_input = [batch_size, embedding dim + 2 * hidden size]
+        # Prepare the input for the RNN by concatenating the embeddings and the context vector
+        combined_input = torch.cat((embeddings, context_vector), dim=1)
+        # combined_input: [batch_size, embedding_dim + 2 * hidden_size]
 
-        # Add a sequence length dimension (since the RNN expects [batch_size, seq_len, input_size])
+        # Apply the linear layer to combine the embeddings and context vector in a learnable fasion
+        rnn_input = torch.tanh(self.input_fc(combined_input))
+        # rnn_input: [batch_size, hidden_size]
+
+        # Add a single time step dimension
         rnn_input = rnn_input.unsqueeze(1)
-        # rnn_input = [batch_size, 1, embedding dim + 2 * hidden size]
+        # rnn_input: [batch_size, 1, hidden_size]
 
         # Run the RNN for a single time step
         output, hidden = self.rnn(rnn_input, hidden)
-        # output = [batch_size, 1, hidden size]
-        # hidden = [num layers, batch_size, hidden size]
+        # output: [batch_size, 1, hidden size]
+        # hidden: [num layers, batch_size, hidden size]
 
         # Remove the sequence length dimension
         output = output.squeeze(1)
-        # output = [batch_size, hidden size]
+        # output: [batch_size, hidden size]
 
         # Generate predictions for the next token
         prediction = self.fc(output)
-        # prediction = [batch_size, vocab size]
+        # prediction: [batch_size, vocab size]
 
         return prediction, hidden, att_weights
